c8bb3464651cbb8308bad14f972bc5e0e32fdbe3
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-12-20 04:17:10 +0000

    blockscope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
-export default function taskBlock (trueOrFalse) {
-  const task = false;
-  const task2 = true;
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    var task = true;
+    var task2 = false;
   }
 
   return [task, task2];
 }
+
