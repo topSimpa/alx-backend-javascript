4e5208ec417bbe9996e017b5ca16d1df4fcd3bf6
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2022-12-21 18:29:59 +0100

    new: added 8-getBudgetCurrentYear.js

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,15 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+  const year = getCurrentYear();
+
+  budget["income-" + year] = income;
+  budget["gddp-" + year] = gdp;
+  budget["capita-"year] = capita;
+
+  return budget;
+}
