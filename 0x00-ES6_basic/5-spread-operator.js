4657fb04c44af3c55a6af5397e48b90a62222d1f
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2022-12-21 15:05:05 +0100

    fix: use the spread operator on array1

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,6 +1,6 @@
 export default function concatArrays(array1, array2, string) {
   const array = [];
-  array.push(array1);
+  array.push(...array1);
   array.push(...array2);
   array.push(...string);
   return array;
