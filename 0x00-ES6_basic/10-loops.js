d4370d9e469f32a9311d0bf81d4522bd15cf4ac4
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2022-12-21 20:52:42 +0100

    fix: replace array with ar

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -3,7 +3,6 @@ export default function appendToEachArrayValue(array, appendString) {
   let val;
   const ar = array;
   for ([idx, val] of array.entries()) {
-    console.log(idx, val);
     ar[idx] = appendString + val;
   }
 
