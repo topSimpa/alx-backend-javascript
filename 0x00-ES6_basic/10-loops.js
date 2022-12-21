893db901126035952c02bd8110ee68b6a2c4ab5a
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2022-12-21 20:56:52 +0100

    refactor: remove unpacking

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
   let val;
   const ar = array;
-  for (val of array.entries) {
+  for (val of array) {
     ar[idx] = appendString + val;
     idx += 1;
   }
