99c850cec1872a0716fdb9ff45251cdb0e11a19f
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2022-12-21 21:09:19 +0100

    refactor: removed all variable definition

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,11 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let idx = 0;
-  let val;
-  const ar = array;
-  for (val of array) {
-    ar[idx] = appendString + val;
-    idx += 1;
+  for (const val of array) {
+    array[array.indexOf(val)] = appendString + val;
   }
 
-  return ar;
+  return array;
 }
