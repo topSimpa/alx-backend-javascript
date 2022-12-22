f6e117d13304e3c6aca28d7a86d1d8e0e7188e11
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2022-12-22 22:23:47 +0100

    new: add 2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = val;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(val) {
+    if (typeof val !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = val;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(val) {
+    if (typeof val !== 'object') {
+      throw TypeError('Students must be an array of strings');
+    }
+    for (const student of val) {
+      if (typeof student !== 'string') {
+        throw TypeError('Students must be an array of strings');
+      }
+    }
+    this._students = val;
+  }
+}
