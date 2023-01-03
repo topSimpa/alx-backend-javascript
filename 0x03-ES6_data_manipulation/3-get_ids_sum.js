bd11646407ce65c116704d02b819e9861d949bf3
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-03 14:38:51 +0100

    refactored: if not available return 0

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,11 +2,12 @@
  * getStudentIdsSum -  returns an array of ids from a list of object
  *
  * @param {array} - array of object
- * @return {array} - array of object id
+ * @return {Number} - array of object id
  */
 
 export default function getListStudentIds(arr) {
   if (Array.isArray(arr)) {
-    return arr.reduce((sum, x) =>sum  + x.id, 0);
+    return arr.reduce((sum, x) => sum + x.id, 0);
   }
+  return 0;
 }
