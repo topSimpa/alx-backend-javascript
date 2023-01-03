513003a83df039fa4b65d4f3d5145af42551b653
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-03 20:16:07 +0100

    new: add 8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,19 @@
+/**
+ * cleanSet - These returns a string
+ *
+ * @param {set} - set, to use
+ * @param {String} - startString, the string to match
+ * @returns {String}
+ */
+
+export default function (set, startString) {
+  const len = startString.length;
+  let str = '';
+  for (const el of set) {
+    if (startString === el.slice(0, len) && len > 0) {
+      str += `${el.slice(len)}-`;
+    }
+  }
+  str = str.slice(0, str.length - 1);
+  return (str);
+}
