d94321d65da055aa455953267112e7181020055a
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-03 18:05:11 +0100

    new: added -set.js

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,10 @@
+/**
+ * setFromArray - create an Array from a set
+ *
+ * @param {Array} - arr, the Array to make set from
+ * @returns {Set}
+ */
+
+export default function (arr) {
+  return new Set(arr);
+}
