d503d8012285d3ad2b0f9e74c1d9a0c9dba94a85
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-04 01:37:10 +0100

    refactor: added conditions

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -9,11 +9,16 @@
 export default function (set, startString) {
   const len = startString.length;
   let str = '';
-  for (let el of set) {
-    if (startString === el.slice(0, len) && len > 0) {
-      str += `${el.slice(len)}-`;
-    }
+  
+  if (set instanceof Set && typeof(startString) === 'string') {
+    for (const el of set) {
+      if (typeof(el) === 'string') {
+        if (startString === el.slice(0, len) && len > 0) {
+          str += `${el.slice(len)}-`;
+        }
+      }
+    }  
+    str = str.slice(0, str.length - 1);
   }
-  str = str.slice(0, str.length - 1);
   return (str);
 }
