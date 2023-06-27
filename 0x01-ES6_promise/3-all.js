247fe00f926ec341ce0f80e7e5ea192597733393
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-06-27 23:54:43 +0100

    tried nesting and chain

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,14 +3,10 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   let str = '';
   const photo = uploadPhoto();
-  photo.then((r) => {
-    str += `${r.body} `;
-  })
-    .catch(() => console.log('Signup system offline'));
-  const profile = createUser();
-  profile.then((r) => {
-    str += `${r.firstName} ${r.lastName}`;
-    console.log(str);
-  })
+  photo.then((r) => createUser()
+    .then((p) => {
+      str += `${r.body} ${p.firstName} ${p.lastName}`;
+    }))
+    .then(() => console.log(str))
     .catch(() => console.log('Signup system offline'));
 }
