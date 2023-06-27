5d5cdd664eec308f84ec28cdb16a095ff53c08d0
A-simpa <abdulmujeebm@yahoo.com>
2023-06-27 02:36:39 +0100

    let see this one

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,10 @@
 export default function iterateThroughObject(reportWithIterator) {
   let str = '';
-  const iter = reportWithIterator[Symbol.iterator]();
   for (const name of reportWithIterator) {
-    str += name;
-    if (!iter.next().done) {
-      str += ' | ';
+	str += name;
+	if (!(reportWithIterator.next().done) {
+		str += ' | ';
+        }
     }
-    iter.next();
-  }
-  return str;
+    return str;
 }
