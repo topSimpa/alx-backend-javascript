92ac13fe295d8c3cabb67ab6e9b0844a5255694f
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-06-27 22:21:02 +0100

    refactor typography error: Success to success

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,12 @@
 export default function iterateThroughObject(reportWithIterator) {
   let str = '';
   const iter = reportWithIterator[Symbol.iterator]();
+  iter.next();
   for (const name of reportWithIterator) {
     str += name;
-    if (!iter.next().done) {
+    if (!(iter.next().done)) {
       str += ' | ';
     }
-    iter.next();
   }
   return str;
 }
