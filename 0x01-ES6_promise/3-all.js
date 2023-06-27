1104793d0ef92eb7b7779202f0c52537f9a21ddc
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-06-28 00:46:10 +0100

    fix: returns a promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   let str = '';
   const photo = uploadPhoto();
-  photo.then((photo) => createUser()
+  return photo.then((foto) => createUser()
     .then((user) => {
-      str += `${photo.body} ${user.firstName} ${user.lastName}`;
+      str += `${foto.body} ${user.firstName} ${user.lastName}`;
       console.log(str);
     }))
     .catch(() => console.log('Signup system offline'));
