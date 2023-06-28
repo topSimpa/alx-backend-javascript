297539065a39db97107db662e27453d01b3b6d76
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-06-28 14:13:13 +0100

    refactor 5-photo-reject: Promise.reject et string interpolation

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,11 +1,3 @@
 export default function uploadPhoto(filename) {
-  const success = false;
-  const promise = new Promise((resolve, reject) => {
-    if (!success) {
-      reject(new Error(`${filename} cannot be processed`));
-    } else {
-      resolve();
-    }
-  });
-  return promise;
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
