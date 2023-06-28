b66a8e6fb2c52c2196e00f59530ca6b01831f46e
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-06-28 19:06:18 +0100

    let see i don understand again

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise = Promise.all(signUpUser(firstName, lastName), uploadPhoto(fileName));
-  return promise.finally((data) => [{
-    status: data.status,
-    value: data.value,
-  }]);
+  return signUpUser(firstName, lastName)
+    .then(() => uploadPhoto(fileName)
+      .catch((promise) => [
+        { status: promise.status, value: promise.value }]));
 }
