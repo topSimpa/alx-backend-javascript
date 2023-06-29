16d55c5176d9efa7c6ecf8e677fc9ecf1979b248
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-06-29 03:40:58 +0100

    allsettled has the status and value fields

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return signUpUser(firstName, lastName)
-    .then(() => uploadPhoto(fileName)
-      .catch((promise) => [
-        { status: promise.status, value: promise.value }]));
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => result);
 }
