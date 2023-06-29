43c6fcb208a2a5b0f9e5fe5670cd17cbe5e751eb
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-06-29 18:03:47 +0100

    return then

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,12 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const prom = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  return prom.map(
-    (promise) => ({
-      status: promise.status,
-      value: (promise.status === 'fulfilled' ? promise.value : promise.reason),
-    }),
-  );
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => res.map(
+      (promise) => ({
+        status: promise.status,
+        value: (promise.status === 'fulfilled' ? promise.value : promise.reason),
+      }),
+    ));
 }
