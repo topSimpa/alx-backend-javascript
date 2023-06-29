f0a9394f1d6d329adce92056c616b9f215328aa8
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-06-29 12:48:49 +0100

    lets see

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,10 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .finally((stats) => stats.map(
+    .then((stats) => stats.map(
       (promise) => ({
         status: promise.status,
-        value: (promise.value === 'fulfilled' ? promise.value : promise.reason),
+        value: (promise.status === 'fulfilled' ? promise.value : promise.reason),
       }),
     ));
 }
