9110c548c814efc2a1cc0681631286af17e43247
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-07-01 12:29:34 +0100

    fix: didnt invoke the functions

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const prom = await Promise.all([uploadPhoto, createUser])
+  const prom = await Promise.all([uploadPhoto(), createUser()])
     .then(
       (res) => ({ photo: res[0], user: res[1] }),
       () => ({ photo: null, user: null }),
