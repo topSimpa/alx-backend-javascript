83c3d0387ab43ae44731b549fb7ac079afab49c6
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-07-01 11:04:38 +0100

    refactor: error message should carry Error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     .then((res) => res.map(
       (promise) => ({
         status: promise.status,
-        value: (promise.status === 'fulfilled' ? promise.value : promise.reason.message),
+        value: (promise.status === 'fulfilled' ? promise.value : `Error: ${promise.reason.message}`),
       }),
     ));
 }
