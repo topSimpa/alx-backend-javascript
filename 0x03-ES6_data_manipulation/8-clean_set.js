676c16ebb8c62405d28bc3ef564be2d8a6788c7a
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-07-05 03:27:05 +0100

    add: added 9-groceries_list.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,6 @@
 /**
- * cleanSet - These returns a string
+ * cleanSet - These search for a startString and appends
+ * each matches suffixes to form a word.
  *
  * @param {set} - set, to use
  * @param {String} - startString, the string to match
@@ -9,16 +10,16 @@
 export default function (set, startString) {
   const len = startString.length;
   let str = '';
-  
-  if (set instanceof Set && typeof(startString) === 'string') {
+
+  if (set instanceof Set && typeof startString === 'string') {
     for (const el of set) {
-      if (typeof(el) === 'string') {
+      if (typeof el === 'string') {
         if (startString === el.slice(0, len) && len > 0) {
           str += `${el.slice(len)}-`;
         }
       }
-    }  
+    }
     str = str.slice(0, str.length - 1);
   }
-  return (str);
+  return str;
 }
