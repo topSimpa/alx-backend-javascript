099f74b45f3d327a8906d62093727d676976828f
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-06-17 20:18:47 +0100

    refactor: added the close on event

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,8 +4,11 @@ const r = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-console.log('Welcome to Holberton School, what is your name?');
-r.question('', (name) => {
+r.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
   r.close();
+});
+
+r.on('close', () => {
+  console.log('This important software is now closing');
 });
