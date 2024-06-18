7eddccbd945b6bad7cfd508b2e95eac3bc12989a
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-06-18 22:12:35 +0100

    refactor: the entire code

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,12 +2,26 @@ const fs = require('fs');
 
 function countStudents(filePath) {
   try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    const studentNumber = data.split('\n').length - 2;
+    let data = fs.readFileSync(filePath, 'utf8');
+    const csStudent = [];
+    const sweStudent = [];
+    data = data.trim().split('\n');
+    const students = data.slice(1);
+    for (const student of students) {
+      const values = student.split(',');
+      if (values[3] === 'CS') {
+        csStudent.push(values[1]);
+      } else {
+        sweStudent.push(values[1]);
+      }
+    }
 
-    console.log(`Number of students: ${studentNumber}`);
+    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students in CS: ${csStudent.length}. List ${csStudent}`);
+    console.log(`Number of students in SWE: ${sweStudent.length}. List ${sweStudent}`);
   } catch (err) {
-    console.error('Cannot load the database');
+    throw new Error('Cannot Load the database');
   }
 }
-countStudents('database.csv');
+
+module.exports = countStudents;
