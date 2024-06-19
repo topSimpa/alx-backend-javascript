cc9804fb0b6609aab186730a01c7979e6ac3f4d6
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-06-19 09:49:26 +0100

    add: added 4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('Hello Holberton School!');
+  res.end();
+}).listen(1245);
+
+module.exports = app;
