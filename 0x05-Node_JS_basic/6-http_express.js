dee2dba4673e43524f314736c9fe5b8c52452434
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-06-19 15:57:47 +0100

    refactor: added newline to print

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,7 +4,9 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(port);
+
+module.exports = app;
