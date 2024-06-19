9baf087fd31bd34141b449036cc169bb27af0226
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-06-19 02:20:27 +0100

    fix: list separated by comma and space during printing

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,18 +7,19 @@ function countStudents(filePath) {
     const sweStudent = [];
     data = data.trim().split('\n');
     const students = data.slice(1);
+
     for (const student of students) {
       const values = student.split(',');
       if (values[3] === 'CS') {
-        csStudent.push(values[1]);
+        csStudent.push(values[0]);
       } else {
-        sweStudent.push(values[1]);
+        sweStudent.push(values[0]);
       }
     }
 
     console.log(`Number of students: ${students.length}`);
-    console.log(`Number of students in CS: ${csStudent.length}. List ${csStudent}`);
-    console.log(`Number of students in SWE: ${sweStudent.length}. List ${sweStudent}`);
+    console.log(`Number of students in CS: ${csStudent.length}. List: ${csStudent.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudent.length}. List: ${sweStudent.join(', ')}`);
   } catch (err) {
     throw new Error('Cannot Load the database');
   }
