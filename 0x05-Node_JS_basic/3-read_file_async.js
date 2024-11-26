5a8d9dc4a2f6114bdf53924d63a9ff955950ab9b
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-26 13:35:30 +0100

    create 3-read_file_async: reading file async

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,48 @@
+/**
+* @file Reading a file synchronously with Node JS
+* @author Simpa
+*/
+
+const { readFile } = require('fs');
+
+function countStudents(path) {
+/**
+* attempts to read the file in path synchronously
+* @param {string} path - address of file to be read
+* @return {Null}
+*/
+
+  let count = 0;
+  const courseCount = {};
+  readFile(path, { encoding: 'utf-8', flag: 'r' }, (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        throw new Error('Cannot load the database');
+      } else {
+        console.log(err);
+      }
+    } else if (data) {
+      const file = data.split('\n');
+      file.forEach((line, ind) => {
+        if (line !== '' && ind !== 0) {
+          count += 1;
+          const linePart = line.split(',');
+          if (linePart[3] in courseCount) {
+            courseCount[linePart[3]].count += 1;
+            courseCount[linePart[3]].names.push(` ${linePart[0]}`);
+          } else {
+            courseCount[linePart[3]] = {};
+            courseCount[linePart[3]].count = 1;
+            courseCount[linePart[3]].names = [linePart[0]];
+          }
+        }
+      });
+      console.log(`Number of students: ${count}`);
+      // eslint-disable-next-line guard-for-in
+      for (const field in courseCount) {
+        console.log(`Number of students in ${field}: ${courseCount[field].count}. List: ${courseCount[field].names}`);
+      }
+    }
+  });
+}
+module.exports = countStudents;
