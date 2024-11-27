2d0d8d51790ffb0a74f4d6a55a0ae2344a12bbb8
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-27 06:23:02 +0100

    create 6-http: added a small server with express

---
+++ 0x05-Node_JS_basic/6-http.js
@@ -0,0 +1,15 @@
+/**
+* @file Create a small HTTP server using Express
+* @author Simpa
+*/
+
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port);
