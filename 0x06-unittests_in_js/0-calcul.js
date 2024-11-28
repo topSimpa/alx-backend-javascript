01e101b6177d60ec22b3214dd2defcd642d7f0ff
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-28 17:03:19 +0100

    changes in package.json

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -3,9 +3,8 @@
 * @author Simpa
 */
 
-
 export default function calculateNumber(a, b) {
-    return Math.round(a) + Math.round(b)
+  return Math.round(a) + Math.round(b);
 }
 
 // console.log(calculateNumber(5, 2));
