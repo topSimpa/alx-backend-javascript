bb044dedeb8a9b3b1dee41ccf801ec54a1e1c286
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-28 16:48:37 +0100

    create calcul & calcul.test: Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,11 @@
+/**
+* @file 0-calcul: Calculate Number Module
+* @author Simpa
+*/
+
+
+export default function calculateNumber(a, b) {
+    return Math.round(a) + Math.round(b)
+}
+
+// console.log(calculateNumber(5, 2));
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,14 @@
+/**
+* @file 0-calcul.test.js: Test for 0-calcul function
+* @author Simpa
+*/
+
+import { assert } from 'assert';
+import calculateNumber from './0-calcul';
+
+assert.equal(calculateNumber(2, 3), 5);
+assert.equal(calculateNumber(2.4, 5.2), 7);
+assert.equal(calculateNumber(2.3, 5.7), 8);
+assert.equal(calculateNumber(2.7, 8.5), 12);
+assert.equal(calculateNumber(3, 2.3), 5);
+assert.equal(calculateNumber(3, 4.7), 8);
