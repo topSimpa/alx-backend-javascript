ce26df46689d71b5b3e1a29becbf3f00ec365e9b
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-29 08:05:00 +0100

    create 4-payment*: using stubs to control function output

---
+++ 0x06-unittests_in_js/4-payment.js
@@ -0,0 +1,14 @@
+/**
+* @file 4-payment: a payment test function
+* @author Simpa
+*/
+
+
+const Utils = require('./utils');
+
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+ console.log(`The total is: ${Utils.calculateNumber('SUM', totalAmount, totalShipping)}`)   
+}
+
+module.exports = sendPaymentRequestToApi
---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,39 @@
+/**
+* @file 3-payments testing
+* @author Simpa
+*/
+
+
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment')
+//const utils = new Utils();
+
+const sinon = require('sinon');
+const {expect} = require('chai')
+let spy, spyConsole;
+
+
+describe('sendPaymentRequestToApi', function() {
+    before(function () {
+        spy = sinon.stub(Utils, 'calculateNumber').callsFake(() => {
+            return 10;
+        })
+        spyConsole = sinon.spy(console, 'log')
+        sendPaymentRequestToApi(100, 20);
+        
+    });
+
+    after( function () {
+        spy.restore();
+    })
+    
+    it("test if console.log was called", function() {    
+        expect(spyConsole.calledWith("The total is: 10")).to.be.true;
+    })
+    
+    it("test what parameters calculateNumber was called with", function() {
+        expect(spy.calledWith("SUM", 100, 20)).to.be.true;
+    })
+     
+    
+})
